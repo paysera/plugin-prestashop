bbeb4b597e3917ce47f0f291e25e9c44ac7e36af
tkasparaitis <t.kasparaitis@evp.lt>
2015-01-23 17:39:24 +0200

    Changed to paysera

---
+++ paysera/translations/en.php
@@ -0,0 +1,18 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paysera}default>configuration_3f86574ad34d16a8cf2ba530c9aafee7'] = 'This module allows you to accept payments by Paysera';
+$_MODULE['<{paysera}default>configuration_b6af2d31afe71aedcceca73cc27fec29'] = 'If the client chooses this payment mode, the order will change its status into a \'Waiting for payment\' status.';
+$_MODULE['<{paysera}default>configuration_aa9f04d1be872e3bdaca1cccf7f02d12'] = 'Payment module details';
+$_MODULE['<{paysera}default>configuration_f5f04b11870f0822cfc263fdc6214528'] = 'Please specify the Paysera details';
+$_MODULE['<{paysera}default>configuration_e082f55a3e947645b3cfc4a3837564be'] = 'Paysera.com project ID:';
+$_MODULE['<{paysera}default>configuration_ae71d10996c375bccc46d24665b318a9'] = 'Paysera.com project passaword:';
+$_MODULE['<{paysera}default>configuration_2f1120281bc59dbf84f4b7d9f210b421'] = 'Test mode:';
+$_MODULE['<{paysera}default>configuration_93cba07454f06a4a960172bbd6e2a435'] = 'Yes';
+$_MODULE['<{paysera}default>configuration_bafd7322c6e97d25b6299b5d6fe8920b'] = 'No';
+$_MODULE['<{paysera}default>configuration_2f8251979372d44fa8e3df67202cbef1'] = 'Display payments list:';
+$_MODULE['<{paysera}default>configuration_49b80feb114d68c0b124c0129536bd02'] = 'Default payment country:';
+$_MODULE['<{paysera}default>configuration_b17f3f4dcf653a5776792498a9b44d6a'] = 'Update settings';
+$_MODULE['<{paysera}default>payment_8d0e94f6e1d030f33f8ce19548051b3d'] = 'Pay by Paysera';
+$_MODULE['<{paysera}default>payment_3af059bc2293e9e46a898bdc659e0a0c'] = 'Pay by Paysera (order process will be faster) 	';
---
+++ paysera/translations/lt.php
@@ -0,0 +1,35 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paysera}default>mokejimai_2d2b05368867f2d9b017e65ce9c1526a'] = 'Paysera';
+$_MODULE['<{paysera}default>mokejimai_e0355bb27ba85e136a8eddc1d95ada91'] = 'Prrimti mokėjimus per Mokėjimai.lt sistemą';
+$_MODULE['<{paysera}default>mokejimai_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Ar tikrai norite ištrinti nustatymus';
+$_MODULE['<{paysera}default>mokejimai_ef6222485ff756bb929a8dfc9c722ea9'] = 'Turi būti suvesti projekto nustatymai, kad veiktų teisingai apmokėjimas.';
+$_MODULE['<{paysera}default>mokejimai_4402acab1c8f90dcf4a31dc96833bd86'] = 'Nepasirinkta valiuta šiame modulyje';
+$_MODULE['<{paysera}default>mokejimai_b4ba0005dcd53932c2b8106963281b62'] = 'Privaloma įvesti projekto ID';
+$_MODULE['<{paysera}default>mokejimai_c888438d14855d7d96a2724ee9c306bd'] = 'Nustatymai atnaujinti';
+$_MODULE['<{paysera}default>configuration_3f86574ad34d16a8cf2ba530c9aafee7'] = 'Šis modulis leidžia priimti mokėjimus per Paysera sistemą.';
+$_MODULE['<{paysera}default>configuration_b6af2d31afe71aedcceca73cc27fec29'] = 'Jeigu klientas pasirinks šį mokėjimo būdą, užsakymas pakeis savo būklę į \"Laukiama\" apmokėjimo';
+$_MODULE['<{paysera}default>configuration_aa9f04d1be872e3bdaca1cccf7f02d12'] = 'Mokėjimo modulio nustatymai';
+$_MODULE['<{paysera}default>configuration_f5f04b11870f0822cfc263fdc6214528'] = 'Įrašykite Paysera modulio duomenis.';
+$_MODULE['<{paysera}default>configuration_e082f55a3e947645b3cfc4a3837564be'] = 'Paysera projekto ID';
+$_MODULE['<{paysera}default>configuration_ae71d10996c375bccc46d24665b318a9'] = 'Paysera projekto slaptažodis:';
+$_MODULE['<{paysera}default>configuration_2f1120281bc59dbf84f4b7d9f210b421'] = 'Testinis režimas?';
+$_MODULE['<{paysera}default>configuration_93cba07454f06a4a960172bbd6e2a435'] = 'Taip';
+$_MODULE['<{paysera}default>configuration_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Ne';
+$_MODULE['<{paysera}default>configuration_2f8251979372d44fa8e3df67202cbef1'] = 'Rodyti mokėjimo būdų pasirinkimą?';
+$_MODULE['<{paysera}default>configuration_49b80feb114d68c0b124c0129536bd02'] = 'Pradinė mokėjimo būdų pasirinkimo šalis';
+$_MODULE['<{paysera}default>configuration_b17f3f4dcf653a5776792498a9b44d6a'] = 'Atnaujinti nustatymus';
+$_MODULE['<{paysera}default>payment_execution_289148aed2fe892082cadcfd5c1e5805'] = 'Paysera mokėjimo būdas';
+$_MODULE['<{paysera}default>payment_execution_f1d3b424cd68795ecaa552883759aceb'] = 'Užsąkymo detalės:';
+$_MODULE['<{paysera}default>payment_execution_e0b4f8216cad5c2c286f08d187503ed0'] = 'Paysera mokėjimo būdas';
+$_MODULE['<{paysera}default>payment_execution_2d2b05368867f2d9b017e65ce9c1526a'] = 'Paysera';
+$_MODULE['<{paysera}default>payment_execution_c047e6ab568a2227653f7d1d30bcf1c2'] = 'Jūs pasirinkote mokėti per Paysera';
+$_MODULE['<{paysera}default>payment_execution_c884ed19483d45970c5bf23a681e2dd2'] = 'Užsąkymo detalės:';
+$_MODULE['<{paysera}default>payment_execution_e2867a925cba382f1436d1834bb52a1c'] = 'Bendra užsąkymo suma';
+$_MODULE['<{paysera}default>payment_execution_48034ce5085e9fd93c65ce59966acf9e'] = 'Pakeikite mokėjimo šalį';
+$_MODULE['<{paysera}default>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'Patvirtinti užsąkymą';
+$_MODULE['<{paysera}default>payment_8d0e94f6e1d030f33f8ce19548051b3d'] = 'Mokėti per Paysera';
+$_MODULE['<{paysera}default>payment_3af059bc2293e9e46a898bdc659e0a0c'] = 'Mokėti per Paysera (užsąkymas bus atliktas greičiau)';
+$_MODULE['<{paysera}default>payment_fb1b6ccdcee225de3d8caf01885d2204'] = 'Toliau >>';
